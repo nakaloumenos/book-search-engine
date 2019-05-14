8cd9dab095c871a821d95988c49f573a32eab882
Nikolaos Alexandros Kaloumenos <nakaloumenos@gmail.com>
2019-05-14 21:39:42 +0100

    configure heroku

--- src/app.js
+++ src/app.js
@@ -16,5 +16,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/searchResult', searchResultRouter);
 app.use('/', indexRouter);
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server listening at: ${port}!`));
