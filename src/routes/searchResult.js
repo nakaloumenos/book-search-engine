97a7096b2da2fa4f3850bbd2c744369a4c11e424
Nikolaos Alexandros Kaloumenos <nakaloumenos@gmail.com>
2019-05-17 23:03:56 +0100

    filter books that are missing any information

--- src/routes/searchResult.js
+++ src/routes/searchResult.js
@@ -7,10 +7,15 @@ router.get('/', async (req, res, next) => {
   try {
     const { queryParams } = req.query;
     const books = await booksRepository.getBooks(queryParams);
-    return res.render('searchResult', { title: 'Book Search Engine', books, query: queryParams });
+    const filteredBooks = books.filter(hasAllInformation);
+    return res.render('searchResult', { title: 'Book Search Engine', books: filteredBooks, query: queryParams });
   } catch (err) {
     return res.render('errorHandler', { title: 'Book Search Engine', errMessage: err.message || 'Uknown' });
   }
 });
+
+const hasAllInformation = (book) => {
+  return book.title != '' && book.author != '' && book.publisher != '' && book.image != '' && book.moreDetails != '';
+};
 
 module.exports = router;
