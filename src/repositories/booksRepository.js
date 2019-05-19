a9fe1ea114bd765cd5388f963f8be61750660497
Nikolaos Alexandros Kaloumenos <nakaloumenos@gmail.com>
2019-05-19 21:48:34 +0100

    count number of results

--- src/repositories/booksRepository.js
+++ src/repositories/booksRepository.js
@@ -5,15 +5,10 @@ const getBooks = async (queryParams) => {
   return axios
     .get('https://www.googleapis.com/books/v1/volumes?q=' + queryParams)
     .then(handleResponse)
-    .catch(handleError)
-    .finally(function() {
-      // always executed
-      console.log('Got here!');
-    });
+    .catch(handleError);
 };
 
 const handleResponse = (res) => {
-  console.log('handling response...');
   const items = res.data.items;
   if (items) {
     return items.map((item) => mapper.mapResponse(item));
@@ -22,14 +17,12 @@ const handleResponse = (res) => {
 };
 
 const handleError = (err) => {
-  console.log('handling error...');
   let errMsg;
   if (err.response) {
     // Request made and server responded
     errMsg = `API returned with ${err.response.status}: ${err.response.data.error.message}`;
   } else if (err.request) {
     // The request was made but no response was received
-    console.log(err.request);
     errMsg = `No response received`;
   } else {
     // Something happened in setting up the request that triggered an Error
