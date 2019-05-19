c032c8bfad1b857fbf8c62400d2740e6a4f2fd2c
Nikolaos Alexandros Kaloumenos <nakaloumenos@gmail.com>
2019-05-19 21:07:13 +0100

    improve error handling and error message. fix bug when query params was a space

--- src/repositories/booksRepository.js
+++ src/repositories/booksRepository.js
@@ -2,19 +2,41 @@ const axios = require('axios');
 const mapper = require('../mappers/bookResponseMapper');
 
 const getBooks = async (queryParams) => {
-  try {
-    const res = await axios.get('https://www.googleapis.com/books/v1/volumes?q=' + queryParams);
-    console.log(res);
-    const items = res.data.items;
+  return axios
+    .get('https://www.googleapis.com/books/v1/volumes?q=' + queryParams)
+    .then(handleResponse)
+    .catch(handleError)
+    .finally(function() {
+      // always executed
+      console.log('Got here!');
+    });
+};
 
-    if (items) {
-      return items.map((item) => mapper.mapResponse(item));
-    }
-
-    return [];
-  } catch (err) {
-    throw new Error(`Error while calling books API`);
+const handleResponse = (res) => {
+  console.log('handling response...');
+  const items = res.data.items;
+  if (items) {
+    return items.map((item) => mapper.mapResponse(item));
   }
+  return [];
+};
+
+const handleError = (err) => {
+  console.log('handling error...');
+  let errMsg;
+  if (err.response) {
+    // Request made and server responded
+    errMsg = `API returned with ${err.response.status}: ${err.response.data.error.message}`;
+  } else if (err.request) {
+    // The request was made but no response was received
+    console.log(err.request);
+    errMsg = `No response received`;
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    errMsg = `Setting request failed`;
+  }
+
+  throw new Error(errMsg);
 };
 
 module.exports = { getBooks };
